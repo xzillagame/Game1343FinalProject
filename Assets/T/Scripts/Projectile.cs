0a900b76d74b0851b461831b799edc8c01d153f2
Xavian Escamilla <xzillagaming@gmail.com>
2024-05-11 00:52:55 -0500

    Implemented Defense Buff

--- Assets/T/Scripts/Projectile.cs
+++ Assets/T/Scripts/Projectile.cs
@@ -38,14 +38,14 @@ public class Projectile : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.layer == 6) ProjectileDestroy();  // Delete later
-
         if (collision.gameObject.layer == bouncyLayer)    
         {
             if (isStretchable) isStretching = !isStretching;
             if (isRotate) rb.angularVelocity = -rb.angularVelocity;
             Helper_SetVelocity(-rb.velocity.x, rb.velocity.y);  
         }
+
+        Destroy(this.gameObject);
     }
 
 
@@ -71,11 +71,6 @@ public class Projectile : MonoBehaviour
         if (isStretching && transform.localScale.x >= strechMaxScale * orgScale.x) isStretching = false;
         else if (!isStretching && transform.localScale.x < orgScale.x) isStretching = true;
     }
-    private void ProjectileDestroy()
-    {
-        Destroy(this.gameObject, 0);
-    }
-
 
     private void Helper_SetVelocity(float x, float y)
     { 
