0b095197d8288b602fbe07c75413283ea2f45d94
kg-programs <kogoebel@my.lonestar.edu>
2024-05-10 23:04:23 -0500

    moving BG

--- Assets/scripts/score.cs
+++ Assets/scripts/score.cs
@@ -7,21 +7,21 @@ using UnityEngine.SceneManagement;
 
 public class score : MonoBehaviour
 {
-    int milesLeft = 1000;
+    int milesLeft = 10000;
     [SerializeField] TMP_Text playerScore;
     [SerializeField] string sceneName;
     void start()
     {
-        milesLeft = 1000;
+        milesLeft = 10000;
         playerScore = GetComponent<TextMeshProUGUI>();
     }
     void Update()
     {
         playerScore.text = "Miles Left: "+ milesLeft.ToString();
         milesLeft -= 1;
-        if(milesLeft < 0)
+        /*if(milesLeft < 0)
         {
             SceneManager.LoadScene(sceneName);
-        }
+        }*/
     }
 }
